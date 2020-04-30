eb7e3dc569e8218972956989394fe801b076c1d3
Judith Huynh <judithuynh@gmail.com>
2020-04-30 02:10:06 +0000

    added quotations around message in property_reviews data

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -21,7 +21,7 @@ VALUES (1, 1, '2018-09-11', '2018-09-26'),
 (2, 1, '2021-10-01', '2021-10-14');
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
-VALUES (2, 3, 1, 3, messages),
-(1, 1, 1, 1, messages),
-(1, 2, 3, 4, messages),
-(4, 3, 2, 1, messages);
+VALUES (2, 3, 1, 3, 'messages'),
+(1, 1, 1, 1, 'messages'),
+(1, 2, 3, 4, 'messages'),
+(4, 3, 2, 1, 'messages');
